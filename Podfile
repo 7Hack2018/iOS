edafdc079ef3cd928d155f6dca33d09a4f654f25
Miguel Dönicke <miguel.doenicke@moguru.de>
2018-10-20 13:48:17 +0200

    play a video

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SevenHack' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SevenHack
+  pod 'youtube-ios-player-helper', '~> 0.1'
+
+end
--- SevenHack/ViewController.swift
+++ SevenHack/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import youtube_ios_player_helper
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var youtubePlayerView: YTPlayerView!
+
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 
+    override func viewDidLoad() {
+        print("viewDidLoad")
+        youtubePlayerView.load(withVideoId: "pIfRdTgy-bc", playerVars: playerVars)
+    }
 
 }
 
+extension ViewController: YTPlayerViewDelegate {
+
+    var playerVars: [AnyHashable: Any] {
+        return [
+            "playsinline": 0
+        ]
+    }
+
+    func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
+        print("playerViewDidBecomeReady")
+    }
+
+    func playerView(_ playerView: YTPlayerView, didChangeTo state: YTPlayerState) {
+        print("didChangeTo state: \(state)")
+    }
+
+    func playerView(_ playerView: YTPlayerView, didChangeTo quality: YTPlaybackQuality) {
+        print("didChangeTo quality: \(quality)")
+    }
+
+    func playerView(_ playerView: YTPlayerView, didPlayTime playTime: Float) {
+        print("playTime: \(playTime)")
+    }
+
+    func playerView(_ playerView: YTPlayerView, receivedError error: YTPlayerError) {
+        print("\(error)")
+    }
+
+}
