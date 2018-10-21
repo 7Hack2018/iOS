b6117d361f1868b8b3139170d987a9e8f4ea8d58
Miguel Dönicke <miguel.doenicke@moguru.de>
2018-10-21 10:13:34 +0200

    Use heartbeat

--- SevenHack/ViewController.swift
+++ SevenHack/ViewController.swift
@@ -13,51 +13,106 @@ import youtube_ios_player_helper
 class ViewController: UIViewController {
 
     @IBOutlet weak var youtubePlayerView: YTPlayerView!
-    @IBOutlet weak var scanButton: UIButton!
+//    @IBOutlet weak var scanButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var noteLabel: UILabel!
+
     private var nfcSession: NFCNDEFReaderSession?
 
-    private let heartbeatInterval = 5.0
+    private let heartbeatInterval = 2.0
     private var heartbeatTimer: Timer?
     private var lastHeartbeat: Double?
     private var untrackedHeartbeatSeconds = 0.0
+
+    private let videos = [
+        "WegyKz5FJC8",
+        "Wg04ktp2Ieg",
+        "aqvqabwx7HU",
+        "6cOOTovIyf8",
+        "WuOpXzQhDFI"
+    ]
 
     override var prefersStatusBarHidden: Bool {
         return true
     }
 
     override func viewDidLoad() {
-        fetchBalance()
+        setUpUI()
         setUpPlayer()
-        loadVideo()
+
+        fetchBalance()
+        loadVideo(number: 1)
     }
 
-    @IBAction func scan(_ sender: Any) {
-        nfcSession = NFCNDEFReaderSession(delegate: self,
-                                          queue: nil,
-                                          invalidateAfterFirstRead: true)
-        nfcSession?.alertMessage = "Let me grab all your private data! 😈"
-        nfcSession?.begin()
+//    @IBAction func scan(_ sender: Any) {
+//        nfcSession = NFCNDEFReaderSession(delegate: self,
+//                                          queue: nil,
+//                                          invalidateAfterFirstRead: true)
+//        nfcSession?.alertMessage = "Let me grab all your private data! 😈"
+//        nfcSession?.begin()
+//    }
+
+    @IBAction func loadVideo(_ sender: UIButton) {
+        guard let text = sender.titleLabel?.text,
+            let number = Int(text) else { return }
+        loadVideo(number: number)
+    }
+
+    private func setUpUI() {
+        display(balance: nil)
+        noteLabel.isHidden = true
+    }
+
+    private func loadVideo(number: Int) {
+        guard number > 0 && number <= videos.count else { return }
+        let index = number - 1
+        let id = videos[index]
+        print("load video: \(number) = video[\(index)] = \(id)")
+        activityIndicator.startAnimating()
+        youtubePlayerView.isHidden = true
+        youtubePlayerView.load(withVideoId: id, playerVars: playerVars)
     }
 
     private func setUpPlayer() {
         youtubePlayerView.delegate = self
     }
 
-    private func loadVideo() {
-        activityIndicator.startAnimating()
-        youtubePlayerView.load(withVideoId: "pIfRdTgy-bc", playerVars: playerVars)
-    }
-
     private func fetchBalance() {
         API.getBalance { result in
             switch result {
-            case .success(let balance):
+            case .success(let balances):
+                guard let balance = balances.first else { break }
+                DispatchQueue.main.async {
+                    self.display(balance: balance)
+                }
                 print("SUCCESS: \(balance)")
             case .failure(let error):
                 print("ERROR: \(error)")
             }
+        }
+    }
+
+    private func secondsToTime (seconds : Int) -> String {
+        let (h, m, s) = (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
+        var time = ""
+        if h > 0 {
+            time += "\(h)h "
+        }
+        if m > 0 {
+            time += "\(m)m "
+        }
+        time += "\(s)s "
+        return time
+    }
+
+    private func display(balance: Balance? = nil) {
+
+        if let time = balance?.limits {
+            timeLabel.text = secondsToTime(seconds: time)
+            timeLabel.isHidden = false
+        } else {
+            timeLabel.isHidden = true
         }
     }
 
@@ -105,6 +160,9 @@ class ViewController: UIViewController {
             switch result {
             case .success(let balance):
                 print("⏲ BALANCE: \(balance)")
+                DispatchQueue.main.async {
+                    self.display(balance: balance)
+                }
             case .failure(let error):
                 print("⏲ ERROR: \(error)")
             }
@@ -117,13 +175,14 @@ extension ViewController: YTPlayerViewDelegate {
 
     var playerVars: [AnyHashable: Any] {
         return [
-            "playsinline": 0
+            "playsinline": 1
         ]
     }
 
     func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
         print("playerViewDidBecomeReady")
         activityIndicator.stopAnimating()
+        youtubePlayerView.isHidden = false
     }
 
     func playerView(_ playerView: YTPlayerView, didChangeTo state: YTPlayerState) {
